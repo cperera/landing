2bc5af98e825cc7a0499778410dce8b705c39c8f
cperera <cperera90+shackday@gmail.com>
2016-12-16 21:32:51 -0800

    Added voronoi diagram example

--- http.py
+++ http.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    name = "Chris"
+    return render_template('hello.html', name=name)
+
+@app.route('/voronoi')
+def voronoi():
+	return render_template('voronoi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
